9f1e7fe7574386254e8d145d56fea43e726b4cab
Dasantha Edirisinghe <t.dasanthaedirisinghe@gmail.com>
2024-03-09 18:57:16 +0530

    make some improvements

--- todo-app/src/app/page.js
+++ todo-app/src/app/page.js
@@ -1,27 +1,81 @@
 "use client"
-import todoForm from "./todoApp/todoForm";
-import todoList from "./todoApp/todoList";
 
 import { useState } from "react";
 
+
 export default function Home() {
-  // Getting the list of all the tasks and saving them
-  const [tasks, setTasks] = useState([])
-    
-  const addTodo = (text) => {
-    const newTodo = {
-      id: Math.random().toString(),
-      text: text,
-    };
-    setTasks([...todos, newTodo]);
-  };
+
+  // tasks handling
+const [task, setTask] = useState('')
+const [tasks, setTasks] = useState([])
+
+// adding the task
+const addTask = (input) => {
+  setTask(input)
+}
+
+// adding the task to the list
+const addTaskToList = () => {
+   if (task !== '') { 
+    const taskItem = { 
+      // when saving to a database change this to a ordered ID
+        // Add a random id which is used to delete 
+        id: Math.random(), 
+
+        // Add a user value to list 
+        value: task, 
+    }; 
+
+    // Update list 
+    setTasks([...tasks, taskItem]); 
+
+    // Reset state 
+    setTask(''); 
+} 
+}
+
+// Function to delete item from list using id to delete 
+const deleteItem = (key) => { 
+  const updatedList =  
+        tasks.filter((item) => item.id !== key); 
+  setTasks(updatedList); 
+}; 
+
+// editing the list
+const editItem = (index) => { 
+  const editedTodo = prompt('Edit the todo:'); 
+  if (editedTodo !== null && editedTodo.trim() !== '') { 
+      const updatedTodos = [...tasks]; 
+      updatedTodos[index].value = editedTodo; 
+      setTasks(updatedTodos); 
+  } 
+}; 
+
 
 
   return (
     <div>
       <h1>Todo List</h1>
-      <todoForm addTodo={addTodo} />
-      <todoList todos={tasks} />
+      <div>
+        <input type="text" placeholder="Add item..." value={task} onChange={(item) => addTask(item.target.value)}/>
+        <button onClick={addTaskToList}>Add</button>
+      </div>
+      {/* showing the list */}
+      <div>
+        {tasks.length > 0 ? (
+          tasks.map((item, index) => (
+            <div key={index}>
+              <span>{item.value}</span>
+              <button onClick={() => deleteItem(item.id)}>Delete</button>
+              <button onClick={() => editItem(index)}>Edit</button>
+            </div>
+          ))
+        ):(
+          <div>
+            No items in the list
+          </div>
+        )}
+      </div>
     </div>
   );
 
--- todo-app/src/app/todoApp/todoList.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const todoList = ({ tasks }) => {
-  return (
-    <div>todoList</div>
-  )
-}
-
-export default todoList
